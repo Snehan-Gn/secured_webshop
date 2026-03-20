c52c2764de2f22c3a21fc5fed0ef4a5477c0dbe2
Snehan-Gn <pr46eml@eduvaud.ch>
2026-03-20 12:14:47 +0100

    feat(login) : ajout du login et prévention de l'injection SQL

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -12,19 +12,19 @@ module.exports = {
             return res.status(400).json({ error: 'Email et mot de passe requis' });
         }
 
-        const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
+        const query = `SELECT * FROM users WHERE email = ? AND password = ?`;
 
-        db.query(query, (err, results) => {
-            if (err) {
-                return res.status(500).json({ error: err.message, query: query });
-            }
+        db.query(query, [email, password], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
 
-            if (results.length === 0) {
-                return res.status(401).json({ error: 'Email ou mot de passe incorrect' });
-            }
+        if (results.length === 0) {
+            return res.status(401).json({ error: 'Email ou mot de passe incorrect' });
+        }
 
-            res.json({ message: 'Connexion réussie', user: results[0] });
-        });
+        res.json({ message: 'Connexion réussie', user: results[0] });
+    });
     },
 
     // ----------------------------------------------------------
